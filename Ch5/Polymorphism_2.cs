0fc18b43ac14f41680f7d3856b32d3f1c3433533
AhnHuiyun <113693706+AhnHuiyun@users.noreply.github.com>
2022-11-28 21:40:23 +0900

    Add files via upload

---
+++ Ch5/Polymorphism_2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Polymorphism_2
+{
+    interface IShape
+    {
+        void Draw();
+    }
+    public class Circle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("I am Circle");
+        }
+        public void CalArea()
+        {
+            Console.WriteLine("PI * R * R");
+        }
+    }
+    public class Rectangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("I am Rectangle");
+        }
+        public void CalArea()
+        {
+            Console.WriteLine("W * H");
+        }
+    }
+    public class Line : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("I am Line");
+        }
+        public void CalArea()
+        {
+            Console.WriteLine("No Area");
+        }
+    }
