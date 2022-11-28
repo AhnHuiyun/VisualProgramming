0ebfeb1638639c61d8f77de68b626c347622eeab
AhnHuiyun <113693706+AhnHuiyun@users.noreply.github.com>
2022-11-28 21:33:28 +0900

    Add files via upload

---
+++ Ch4/CSharp_Auto_Property.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CSharp_Auto_Property
+{
+    class NameCard
+    {
+        public string Name
+        {
+            get; set;
+        }
+        public string PhoneNumber
+        {
+            get; set;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameCard nc = new NameCard();
+            nc.Name = "홍길동";
+            nc.PhoneNumber = "010-1111-0000";
+            Console.WriteLine("Name={0}, Phone_Number={1}", nc.Name, nc.PhoneNumber);
+        }
+    }
+}
