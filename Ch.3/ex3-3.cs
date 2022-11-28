8d08cd3966c7ab726bf61b69ece2093e278e7387
AhnHuiyun <113693706+AhnHuiyun@users.noreply.github.com>
2022-11-28 21:28:07 +0900

    Add files via upload

---
+++ Ch.3/ex3-3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class AnotherBlock
+{
+    public static void Main(string[] args)
+    {
+        int x = 1;
+        {
+            // int x; // error ( 블록 밖의 변수와 겹침)
+            //메소드 내 지역변수 이름은 블록 밖에 선언된 변수들의 이름과 달라야 함
+            int y = 2;
+            Console.WriteLine("Block 1: x = " + x + ", y = " + y);
+        }
+        {
+            int y = 3;
+            Console.WriteLine("Block 2: x = " + x + ", y = " + y);
+        }
+    }
+}
