be2bc603386ec5914d2d0c42eb267e9749a8a844
AhnHuiyun <113693706+AhnHuiyun@users.noreply.github.com>
2022-11-28 21:23:39 +0900

    Add files via upload

---
+++ Ch2/ex2-18.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//class IncDecOperatorsApp
+//{
+//    public static void Main()
+//    {
+//        int x = 3, y = 5;
+//        int a, b;
+//        ++x; --y;
+//        Console.WriteLine("x = " + x + " , y = " + y);
+//        a = (++x) + 1;
+//        Console.WriteLine("x = " + x + " , a = " + a);
+//        b = (y++) + 1;
+//        Console.WriteLine("y = " + y + " , b = " + b);
+//    }
+//}
+
