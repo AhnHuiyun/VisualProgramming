33048e98ea89fc0a313a8806015597a6ababcca5
AhnHuiyun <113693706+AhnHuiyun@users.noreply.github.com>
2022-11-28 21:20:50 +0900

    Add files via upload

---
+++ Filestream/save_hi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Filestream
+{
+    internal class save_hi
+    {
+        static void Main()
+        {
+            FileStream fs;
+            try
+            {
+                fs = new FileStream("file2.txt", FileMode.Create);
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("파일을 열 수 없습니다.");
+                return;
+            }
+            StreamWriter w = new StreamWriter(fs);
+            w.Write("안녕하세요. C#");
+            w.Close();
+            Console.WriteLine("안녕하세요. C#");
+        }
+
+    }
+}
+//( ../bin/Debug 폴더)에 file2 저장된 거 확인하기
