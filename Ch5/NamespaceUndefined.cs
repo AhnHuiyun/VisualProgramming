6e2828d04d8fe5d236d6525330b429ce043e1d5f
AhnHuiyun <113693706+AhnHuiyun@users.noreply.github.com>
2022-11-28 21:40:02 +0900

    Add files via upload

---
+++ Ch5/NamespaceUndefined.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace NamespaceTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            global::test x = new global::test();
+            // var x = new global::test();
+            x.SayHello();
+            test y = new test();
+            y.SayHello();
+        }
+    }
+}
+// namespace-less test class
+public class test
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+namespace NamespaceTests
+{
+    public class test
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello Moon....");
+        }
+    }
+}
+//Namespace에 속하지않는 class(➔ default로global namespace에속함 )
+
