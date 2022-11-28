88f8f2c8c635b157f8b5d523edfd7a44e246daaa
AhnHuiyun <113693706+AhnHuiyun@users.noreply.github.com>
2022-11-28 21:39:07 +0900

    Add files via upload

---
+++ Ch5/Shape(class_library)/Rectangle.cs
@@ -0,0 +1,12 @@
+﻿namespace Shape
+{
+    public class Rectangle
+    {
+        public int height;
+        public int width;
+        override public string ToString()
+        {
+            return string.Format("Width : {0}, Height : {1}", width, height);
+        }
+    }
+}
