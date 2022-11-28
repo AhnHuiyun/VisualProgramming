9c44ac1403dff94413d17c3cc522aedcce8f62e5
AhnHuiyun <113693706+AhnHuiyun@users.noreply.github.com>
2022-11-28 21:38:29 +0900

    Add files via upload

---
+++ Ch5/NamespaceApp(Dynamic_Link_Library)/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Shape; // 사용하고자 하는 DLL의 네임스페이스 명시
+//오류가 발생-> 프로젝트-> 참조 추가->찾아보기-> …. \Shape\bin\Debug\Shape.dll->추가
+public class NamespaceApp
+{
+    public static void Main()
+    {
+        Rectangle rect = new Rectangle();
+        rect.width = 10;
+        rect.height = 20;
+        Console.WriteLine("rect : " + rect);
+    }
+}
