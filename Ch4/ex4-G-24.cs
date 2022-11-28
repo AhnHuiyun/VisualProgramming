0512301616eaaddf24d5b0d116a7eebded316729
AhnHuiyun <113693706+AhnHuiyun@users.noreply.github.com>
2022-11-28 21:35:59 +0900

    Add files via upload

---
+++ Ch4/ex4-G-24.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+class ClickEventApp : Form
+{
+    public ClickEventApp()
+    { // 생성자
+        this.Text = "ClickEventApp";
+        this.Click += new EventHandler(ClickEvent); //  등록
+    }
+    // ... 이벤트 처리기 ... 
+    private void ClickEvent(object sender, EventArgs e)
+    {
+        MessageBox.Show(" sender = " + sender.GetType());
+    }
+    public static void Main()
+    {
+        Application.Run(new ClickEventApp());
+    }
+}
