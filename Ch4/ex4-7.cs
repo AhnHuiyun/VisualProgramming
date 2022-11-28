5608f2e42f0f0bef6dc68d220f4a7e1ef3540f16
AhnHuiyun <113693706+AhnHuiyun@users.noreply.github.com>
2022-11-28 21:34:02 +0900

    Add files via upload

---
+++ Ch4/ex4-7.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class ParameterArrayApp
+{
+    static void ParameterArray(params object[] obj)
+    {
+        for (int i = 0; i < obj.Length; i++)
+            Console.WriteLine(obj[i]);
+    }
+    public static void Main()
+    {
+        ParameterArray(123, "Hello", true, 'A');
+    }
+}
+//지원되는 건 안되 남용은 x
