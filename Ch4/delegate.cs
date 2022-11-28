4306796a7fe64ef1494238b4f2169af13d14e117
AhnHuiyun <113693706+AhnHuiyun@users.noreply.github.com>
2022-11-28 21:33:35 +0900

    Add files via upload

---
+++ Ch4/delegate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public delegate void MyDelegate(string msg);
+class Program
+{
+    public static void Main()
+    {
+        MyDelegate del = ClassA.MethodA; // MyDelegate del = new MyDelegate(ClassA.MethodA);
+        InvokeDelegate(del);
+        del = ClassB.MethodB; // MyDelegate del = new MyDelegate(ClassB.MethodB);
+        InvokeDelegate(del);
+        del = (string msg) => Console.WriteLine("Called lambda expression: " + msg);
+        InvokeDelegate(del);
+    }
+    public static void InvokeDelegate(MyDelegate del)
+    {
+        del("Hello World");
+    }
+}
+class ClassA
+{
+    public static void MethodA(string message)
+    {
+        Console.WriteLine("Called ClassA.MethodA() with parameter: " + message);
+    }
+}
+class ClassB
+{
+    public static void MethodB(string message)
+    {
+        Console.WriteLine("Called ClassB.MethodB() with parameter: " + message);
+    }
+}
