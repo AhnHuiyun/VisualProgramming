c64ad53ffc0ddec90348ea4b73b313a598820edb
AhnHuiyun <113693706+AhnHuiyun@users.noreply.github.com>
2022-11-28 21:28:14 +0900

    Add files via upload

---
+++ Ch.3/ex3-5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class IfStApp
+{
+    public static void Main()
+    {
+        int n;
+        Console.Write("Enter a number = ");
+        n = Console.Read() - '0';
+        if (n % 2 == 0) 
+            Console.WriteLine(n + " is an even number.");
+        if (n % 2 != 0)
+            Console.WriteLine(n + " is an odd number.");
+    }
+}
+//2가지 경우만 존재하는데 2개의 if문 사용은 비효율적
+//if ~else문으로 바꾸어 주어야함
