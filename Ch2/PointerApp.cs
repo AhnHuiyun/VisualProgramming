0dc368a8a19865c872c3183b345474a25f8a810d
AhnHuiyun <113693706+AhnHuiyun@users.noreply.github.com>
2022-11-28 21:24:01 +0900

    Add files via upload

---
+++ Ch2/PointerApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class PointerApp
+{
+    unsafe public static void Swap(int* px, int* py)
+    {
+        int tmp = *px;
+        *px = *py;
+        *py = tmp;
+    }
+    public static void Main()
+    {
+        int x = 1, y = 2;
+        Console.WriteLine("Before : x = " + x + ", y = " + y);
+        unsafe
+        {
+            Swap(&x, &y);
+        }
+        Console.WriteLine(" After : x = " + x + ", y = " + y);
+    }
+}
+//프로젝트->속성->빌드*->안전하지 않은 코드 허용 체크
